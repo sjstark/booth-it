242233d509e0c4c1b80e9de1807c70d2d2025945
Sam <samstarkscenic@gmail.com>
2021-01-05 16:09:35 -0500

    restructured flask app

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, request, session
 from flask_cors import CORS
-from flask_migrate import flask_migrate
+from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 
@@ -42,3 +42,12 @@ def inject_csrf_token(response):
                         httponly=True
                         )
     return response
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def react_root(path):
+    print("path", path)
+    if path == "favicon.ico":
+        return app.send_static_file("favicon.ico")
+    return app.send_static_file("index.html")
