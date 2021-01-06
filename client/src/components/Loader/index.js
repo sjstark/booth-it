513708c958412cccf325b1320e3e9421e5c57437
Sam <samstarkscenic@gmail.com>
2021-01-06 02:40:02 -0500

    completes the loader spinner

--- client/src/components/Loader/index.js
+++ client/src/components/Loader/index.js
@@ -1,50 +1,136 @@
-import React from 'react'
-import { useSpring, animated, config } from 'react-spring'
+import React, { useState, useRef } from 'react'
+import { useSpring, animated, config, useChain } from 'react-spring'
 
 import "./Loader.css"
 
+const ANIMATION_DURATION = 3000
+
+function interpRadian(r, max) {
+  let num = (max * Math.sin(r + (2 * Math.PI) / 1.6))
+  return num > 0 ? num : 0
+}
+
 function TopLeft() {
+  const bounceInterp = r => {
+    return `translate(${-1 * interpRadian(r, 16)}%, ${-1 * interpRadian(r, 25)}%)`
+  }
+
+  const { radians } = useSpring({
+    from: {
+      radians: 0
+    },
+    to: async next => {
+      while (true) {
+        await next({
+          radians: 2 * Math.PI
+        })
+      }
+    },
+    reset: true,
+    config: {
+      duration: ANIMATION_DURATION
+    }
+  })
+
+
   return (
-    <div className="logo-path__hexagon logo-path__hexagon--TL">
-      <div className="logo-path__hexagon-color" />
-    </div>
+    <animated.div style={{ transform: radians.interpolate(bounceInterp) }} className="loader__hexagon loader__hexagon--TL" >
+      <div className="loader__hexagon-color" />
+    </animated.div>
   )
 }
 
 function BottomLeft() {
+  const bounceInterp = r => {
+    return `translate(${-1 * interpRadian(r, 16)}%, ${interpRadian(r, 25)}%)`
+  }
+
+  const { radians } = useSpring({
+    from: {
+      radians: 0
+    },
+    to: async next => {
+      while (true) {
+        await next({
+          radians: 2 * Math.PI
+        })
+      }
+    },
+    reset: true,
+    config: {
+      duration: ANIMATION_DURATION
+    }
+  })
+
+
   return (
-    <div className="logo-path__hexagon logo-path__hexagon--BL">
-      <div className="logo-path__hexagon-color" />
-    </div>
+    <animated.div style={{ transform: radians.interpolate(bounceInterp) }} className="loader__hexagon loader__hexagon--BL">
+      <div className="loader__hexagon-color" />
+    </animated.div>
   )
 }
 
 function Right() {
+  const bounceInterp = r => {
+    return `translate(${interpRadian(r, 33)}%, 0%)`
+  }
+
+  const { radians } = useSpring({
+    from: {
+      radians: 0
+    },
+    to: async next => {
+      while (true) {
+        await next({
+          radians: 2 * Math.PI
+        })
+      }
+    },
+    reset: true,
+    config: {
+      duration: ANIMATION_DURATION
+    }
+  })
+
+
   return (
-    <div className="logo-path__hexagon logo-path__hexagon--R">
-      <div className="logo-path__hexagon-color" />
-    </div>
+    <animated.div style={{ transform: radians.interpolate(bounceInterp) }} className="loader__hexagon loader__hexagon--R">
+      <div className="loader__hexagon-color" />
+    </animated.div>
   )
 }
 
 export default function Loader() {
+
+
   const props = useSpring({
-    transform: "rotate(720deg)",
-    width: "100%",
-    height: "100%",
     from: {
-      transform: "rotate(0deg)"
+      transform: "rotate(0deg)",
+      width: "100%",
+      height: "100%",
     },
-    config: config.molasses
+    to: async next => {
+      while (true) {
+        await next({
+          transform: "rotate(360deg)",
+          width: "100%",
+          height: "100%",
+        })
+      }
+    },
+    reset: true,
+    config: {
+      duration: ANIMATION_DURATION
+    }
   })
 
+
+
   return (
-    <animated.div style={props}>
-      <section className="logo-path__logo-container">
-        <TopLeft />
-        <BottomLeft />
-        <Right />
-      </section>
+    <animated.div style={props} className="loader__logo-container">
+      <TopLeft />
+      <BottomLeft />
+      <Right />
     </animated.div>
   )
 }
