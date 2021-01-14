8ba97890826241dff0b09cad06de5f8f854b9b69
Sam <samstarkscenic@gmail.com>
2021-01-14 14:36:12 -0500

    changed back to module, uses gunicorn and eventlet to launch production

--- server/__init__.py
+++ server/__init__.py
@@ -19,97 +19,95 @@ from .config import Config
 socketio = SocketIO()
 
 
-def create_app(debug=False):
-    from .api.auth_routes import auth_routes
-    from .api.show_routes import show_routes
-    from .api.invite_routes import invite_routes
-    from .api.messenger import messenger_bp
+# def create_app(debug=False):
+from .api.auth_routes import auth_routes
+from .api.show_routes import show_routes
+from .api.invite_routes import invite_routes
+from .api.messenger import messenger_bp
 
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    # Set the app into debug mode to refresh on detected changes
-    app.debug = debug
+# # Set the app into debug mode to refresh on detected changes
+# app.debug = debug
 
-    # Flask Login Manager Setup
-    login = LoginManager(app)
-    login.login_view = "auth.unauthorized"
-
-
-    @login.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+# Flask Login Manager Setup
+login = LoginManager(app)
+login.login_view = "auth.unauthorized"
 
 
-    @login.unauthorized_handler
-    def unauth_handler():
-        return {'errors': ['Unauthorized']}, 401
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
-    # Integrate flask seeding here eventually
-    app.cli.add_command(seed_commands)
-
-    app.config.from_object(Config)
-
-    # Init db with app here
-    db.init_app(app)
-    Migrate(app, db)
-
-    # Socket IO for messages
-    app.register_blueprint(messenger_bp)
-
-    # @socketio.on("connection")
-    # def joined(message):
-    #     print('New client connected')
-
-    # @socketio.on("message")
-    # def handle_message(message):
-    #     print(message)
-    #     send(message, broadcast=True)
-    #     return None
-
-    socketio.init_app(app, cors_allowed_origins="*")
-
-    app.register_blueprint(auth_routes, url_prefix='/api/auth')
-    app.register_blueprint(show_routes, url_prefix='/api/shows')
-    app.register_blueprint(invite_routes, url_prefix='/api/invites')
-
-    # Application Security
-    CORS(app)
+@login.unauthorized_handler
+def unauth_handler():
+    return {'errors': ['Unauthorized']}, 401
 
 
-    # Since we are deploying with Docker and Flask,
-    # we won't be using a buildpack when we deploy to Heroku.
-    # Therefore, we need to make sure that in production any
-    # request made over http is redirected to https.
-    # Well.........
-    @app.before_request
-    def https_redirect():
-        if os.environ.get('FLASK_ENV') == 'production':
-            if request.headers.get('X-Forwarded-Proto') == 'http':
-                url = request.url.replace('http://', 'https://', 1)
-                code = 301
-                return redirect(url, code=code)
+# Integrate flask seeding here eventually
+app.cli.add_command(seed_commands)
+
+app.config.from_object(Config)
+
+# Init db with app here
+db.init_app(app)
+Migrate(app, db)
+
+# Socket IO for messages
+app.register_blueprint(messenger_bp)
+
+# @socketio.on("connection")
+# def joined(message):
+#     print('New client connected')
+
+# @socketio.on("message")
+# def handle_message(message):
+#     print(message)
+#     send(message, broadcast=True)
+#     return None
+
+socketio.init_app(app, cors_allowed_origins="*")
+
+app.register_blueprint(auth_routes, url_prefix='/api/auth')
+app.register_blueprint(show_routes, url_prefix='/api/shows')
+app.register_blueprint(invite_routes, url_prefix='/api/invites')
+
+# Application Security
+CORS(app)
 
 
-    @app.after_request
-    def inject_csrf_token(response):
-        response.set_cookie('csrf_token',
-                            generate_csrf(),
-                            secure=True if os.environ.get(
-                                "FLASK_ENV") == "production" else False,
-                            samesite="Strict" if os.environ.get(
-                                "FLASK_ENV") == "production" else None,
-                            httponly=True
-                            )
-        return response
+# Since we are deploying with Docker and Flask,
+# we won't be using a buildpack when we deploy to Heroku.
+# Therefore, we need to make sure that in production any
+# request made over http is redirected to https.
+# Well.........
+@app.before_request
+def https_redirect():
+    if os.environ.get('FLASK_ENV') == 'production':
+        if request.headers.get('X-Forwarded-Proto') == 'http':
+            url = request.url.replace('http://', 'https://', 1)
+            code = 301
+            return redirect(url, code=code)
 
 
-    @app.route("/", defaults={"path": ""})
-    @app.route("/<path:path>")
-    def react_root(path):
-        print("path", path)
-        if path == "favicon.ico":
-            return app.send_static_file("favicon.ico")
-        return app.send_static_file("index.html")
+@app.after_request
+def inject_csrf_token(response):
+    response.set_cookie('csrf_token',
+                        generate_csrf(),
+                        secure=True if os.environ.get(
+                            "FLASK_ENV") == "production" else False,
+                        samesite="Strict" if os.environ.get(
+                            "FLASK_ENV") == "production" else None,
+                        httponly=True
+                        )
+    return response
 
-    return app
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def react_root(path):
+    print("path", path)
+    if path == "favicon.ico":
+        return app.send_static_file("favicon.ico")
+    return app.send_static_file("index.html")
