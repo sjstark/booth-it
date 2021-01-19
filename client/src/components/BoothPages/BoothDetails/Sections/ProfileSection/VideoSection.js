d699e57ee0e65d7c967e924db8a93e48a6c93603
Sam <samstarkscenic@gmail.com>
2021-01-18 20:34:41 -0500

    changes screen records to gif

--- client/src/components/BoothPages/BoothDetails/Sections/ProfileSection/VideoSection.js
+++ client/src/components/BoothPages/BoothDetails/Sections/ProfileSection/VideoSection.js
@@ -38,6 +38,7 @@ export default function VideoSection({ content, setContent, editting }) {
       )}
       <ReactPlayer
         width="100%"
+        height="fit-content"
         url={videoUrl}
       />
     </>
